c3a8a98af8fd0fe12a03c6d2ba5a87f833740799
wekotsai <42733086+wekotsai@users.noreply.github.com>
2018-08-27 14:02:53 +0200

    Filters are working

--- senate-data.js
+++ senate-data.js
@@ -1,37 +1,38 @@
 console.log(data.results[0].members);
-
-        var myTable = document.querySelector("#senate-data");
-        myTable.classList.add("table", "table-hover");
-
-        var myTBody = document.createElement("tbody");
-
-        for (var i = 0; i < data.results[0].members.length; i++) {
-            var everyMember = data.results[0].members[i];
-            var myTr = document.createElement("tr");
-
-            //alternative solution
-            //            var a = [data.results[0].members[i].first_name]; 
-            //            var b = [data.results[0].members[i].middle_name];
-            //            var c = [data.results[0].members[i].last_name];
-            //            var d = a.concat(b, c);
-
-            var names = [everyMember.first_name, everyMember.middle_name, everyMember.last_name];
-            var full_name = names.join(" ");
+//dropdown filter
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
 
 
-            var url = everyMember.url;
-            var name_with_link = "<a href=\"" + url + "\">" + full_name + "</a>";
-            //            console.log(name_with_link);
+//table
+let myTable = document.querySelector("#senate-data");
+myTable.classList.add("table", "table-hover");
 
-            myTr.insertCell().innerHTML = name_with_link;
-            myTr.insertCell().innerHTML = everyMember.party;
-            myTr.insertCell().innerHTML = everyMember.state;
-            myTr.insertCell().innerHTML = everyMember.seniority;
-            myTr.insertCell().innerHTML = everyMember.votes_with_party_pct + '%';
-            myTBody.append(myTr);
-        }
+let myTBody = document.createElement("tbody");
 
-        myTable.append(myTBody);
+handleChange();
+
+//for (var i = 0; i < data.results[0].members.length; i++) {
+//    var everyMember = data.results[0].members[i];
+//    var myTr = document.createElement("tr");
+//
+//    var names = [everyMember.first_name, everyMember.middle_name, everyMember.last_name];
+//    var full_name = names.join(" ");
+//
+//
+//    var url = everyMember.url;
+//    var name_with_link = "<a href=\"" + url + "\">" + full_name + "</a>";
+//    //            console.log(name_with_link);
+//
+//    myTr.insertCell().innerHTML = name_with_link;
+//    myTr.insertCell().innerHTML = everyMember.party;
+//    myTr.insertCell().innerHTML = everyMember.state;
+//    myTr.insertCell().innerHTML = everyMember.seniority;
+//    myTr.insertCell().innerHTML = everyMember.votes_with_party_pct + '%';
+//    myTBody.append(myTr);
+//}
+//
+//myTable.append(myTBody);
 
 //checkbox
 //document.getElementById("rep").addEventListener("click", selectParty);
@@ -42,19 +43,24 @@ console.log(data.results[0].members);
 
 function handleChange() {
     
+    myTBody.innerHTML = "";
+
     var myChecked = Array.from(document.querySelectorAll('input.party:checked'));
-    
+
     var myCheckedValues = [];
     myChecked.forEach(checkbox => {
         myCheckedValues.push(checkbox.value);
     });
-    console.log(myCheckedValues);
     
-    myTBody.innerHTML = "";
-    
+    let selector = document.getElementById("stateSelect");
+
+
     data.results[0].members.forEach(member => {
-        
-        if(myCheckedValues.includes(member.party) || myCheckedValues.length == 0){
+
+        let partyFilter = myCheckedValues.includes(member.party) || myCheckedValues.length == 0;
+        let statesFilter = (member.state == selector.value) || selector.value == "all";
+
+        if (partyFilter && statesFilter) {
             var myTr = document.createElement("tr");
             var names = [member.first_name, member.middle_name, member.last_name];
             var full_name = names.join(" ");
@@ -69,11 +75,11 @@ function handleChange() {
             myTr.insertCell().innerHTML = member.votes_with_party_pct + '%';
             myTBody.append(myTr);
         }
-    })
+    });
     
+    myTable.append(myTBody);
     
-    
-    
+
 }
 
 //function filterByParty() {
@@ -82,10 +88,10 @@ function handleChange() {
 
 
 
-        //alternative solution 
-        //            var cellName = document.createElement("td");
-        //            cellName.innerHTML = data.results[0].members[i].first_name;
-        //            tr.append(cellName);
+//alternative solution 
+//            var cellName = document.createElement("td");
+//            cellName.innerHTML = data.results[0].members[i].first_name;
+//            tr.append(cellName);
 
 
-        //document.getElementById("senate-data").innerHTML = JSON.stringify(data,null,2);
+//document.getElementById("senate-data").innerHTML = JSON.stringify(data,null,2);
