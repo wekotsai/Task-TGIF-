8464003dc855c0c299126962807434b8af83bffb
wekotsai <42733086+wekotsai@users.noreply.github.com>
2018-09-05 09:52:25 +0200

    house date live

--- house-data.js
+++ house-data.js
@@ -1,105 +1,61 @@
-console.log(data.results[0].members);
-
-var myTable = document.querySelector("#house-data");
-
-myTable.classList.add("table", "table-hover");
-
-var myTBody = document.createElement("tbody");
-
-handleChange();
-//1. all states with duplicates
-var allStates = data.results[0].members.map(oneMember => oneMember.state);
-console.log(allStates);
-//2. remove duplicates
-var allStatesNoDupes = [];
-allStates.forEach(oneState => {
-    if (!allStatesNoDupes.includes(oneState)) {
-        allStatesNoDupes.push(oneState);
-    }
-});
-
-var selector = document.getElementById("stateSelect");
-
-allStatesNoDupes.sort();
-allStatesNoDupes.forEach(aState => {
-
-    var newOption = document.createElement("option");
-    console.log('one done')
-    newOption.innerHTML = aState;
-    selector.append(newOption);
-})
+function main() { 
+    
+    let myTable = document.querySelector("#house-data");
 
 
-function handleChange() {
+    let myTBody = document.createElement("tbody");
 
-    myTBody.innerHTML = "";
+    fetch("https://api.propublica.org/congress/v1/113/house/members.json", {
+            headers: new Headers({
+                "X-API-Key": "vtWikQYFzHHmeKCeyC4McaUxPPDQGDGuSxjOOLFc"
+            })
+        })
+    
+        .then(response => response.json())
+        .then(jsonData => {
 
-    let myChecked = Array.from(document.querySelectorAll('input.party:checked'));
-
-    let myCheckedValues = [];
-    myChecked.forEach(checkbox => {
-        myCheckedValues.push(checkbox.value);
-    });
-
-    let selector = document.getElementById("stateSelect");
-
-
-    data.results[0].members.forEach(member => {
-
-        let partyFilter = myCheckedValues.includes(member.party) || myCheckedValues.length == 0;
-        let statesFilter = (member.state == selector.value) || selector.value == "all";
-
-        if (partyFilter && statesFilter) {
-            var myTr = document.createElement("tr");
-            var names = [member.first_name, member.middle_name, member.last_name];
-            var full_name = names.join(" ");
-            var url = member.url;
-
-            myTr.insertCell().innerHTML = full_name.link(url);
-            myTr.insertCell().innerHTML = member.party;
-            myTr.insertCell().innerHTML = member.state;
-            myTr.insertCell().innerHTML = member.seniority;
-            myTr.insertCell().innerHTML = member.votes_with_party_pct + '%';
-            myTBody.append(myTr);
-        }
-    });
-
-    myTable.append(myTBody);
-
-
+            myFirstVueObject(jsonData.results[0].members)
+        })
 }
 
-//        for (var i = 0; i < data.results[0].members.length; i++) {
-//            var myTr = document.createElement("tr");
-//
-//            //alternative solution
-//            //            var a = [data.results[0].members[i].first_name]; 
-//            //            var b = [data.results[0].members[i].middle_name];
-//            //            var c = [data.results[0].members[i].last_name];
-//            //            var d = a.concat(b, c);
-//
-//            var names = [data.results[0].members[i].first_name, data.results[0].members[i].middle_name, data.results[0].members[i].last_name];
-//            var full_name = names.join(" ");
-//
-//
-//            var url = data.results[0].members[i].url;
-//            var name_with_link = "<a href=\"" + url + "\">" + full_name + "</a>";
-//            //            console.log(name_with_link);
-//
-//            myTr.insertCell().innerHTML = name_with_link;
-//            myTr.insertCell().innerHTML = data.results[0].members[i].party;
-//            myTr.insertCell().innerHTML = data.results[0].members[i].state;
-//            myTr.insertCell().innerHTML = data.results[0].members[i].seniority;
-//            myTr.insertCell().innerHTML = data.results[0].members[i].votes_with_party_pct + '%';
-//            myTBody.append(myTr);
-//        }
-//
-//        myTable.append(myTBody);
-//
-//        //alternative solution 
-//        //            var cellName = document.createElement("td");
-//        //            cellName.innerHTML = data.results[0].members[i].first_name;
-//        //            tr.append(cellName);
-//
-//
-//        //document.getElementById("senate-data").innerHTML = JSON.stringify(data,null,2);
+
+function myFirstVueObject(myData) {
+
+    console.log(myData);
+
+    new Vue({
+            el: '#app',
+            data: {
+                myVueMembers: myData,
+                backupMembers: myData,
+                filteredMembers: [],
+                checkedNames: [],
+                stateNameArray: [],
+                stateValue: 'all'
+            },
+            created() {
+                this.createStateNameArray();
+            },
+            methods: {
+                partyFilter: function () {
+        // use 'backupMembers' to store the originl array 
+                    this.myVueMembers = this.backupMembers.filter(member => {
+        // use || to restore the table after unchecking
+                        let partyFilter = this.checkedNames.includes(member.party) || this.checkedNames.length == 0;
+                        let stateFilter = this.stateValue == member.state || this.stateValue == 'all';
+
+                        return partyFilter && stateFilter;
+                    });
+
+                },
+                createStateNameArray: function(){
+                    let dupsArray = this.myVueMembers.map(member => member.state);
+                    dupsArray.forEach(stateName => {
+                        if(!this.stateNameArray.includes(stateName)){
+                            this.stateNameArray.push(stateName);
+                        }
+                    })
+                }
+            }
+    })
+}
--- senate-data.js
+++ senate-data.js
@@ -1,49 +1,23 @@
-var data;
+function main() { 
+    
+    let myTable = document.querySelector("#senate-data");
+    myTable.classList.add("table", "table-hover");
 
-//onload = (function(){
+    let myTBody = document.createElement("tbody");
 
-
-let myTable = document.querySelector("#senate-data");
-myTable.classList.add("table", "table-hover");
-
-let myTBody = document.createElement("tbody");
-
-fetch("https://api.propublica.org/congress/v1/113/senate/members.json", {
-        headers: new Headers({
-            "X-API-Key": "vtWikQYFzHHmeKCeyC4McaUxPPDQGDGuSxjOOLFc"
+    fetch("https://api.propublica.org/congress/v1/113/senate/members.json", {
+            headers: new Headers({
+                "X-API-Key": "vtWikQYFzHHmeKCeyC4McaUxPPDQGDGuSxjOOLFc"
+            })
         })
-    })
-    //    .then(json => json.json())
-    //    .then(data => {
-    .then(response => response.json())
-    .then(jsonData => {
+    
+        .then(response => response.json())
+        .then(jsonData => {
 
-        data = jsonData;
-
-        myFirstVueObject(jsonData.results[0].members)
-//        handleChange();
-
-        //1. all states with duplicates
-        var allStates = data.results[0].members.map(oneMember => oneMember.state);
-        //2. remove duplicates
-        var allStatesNoDupes = [];
-        allStates.forEach(oneState => {
-            if (!allStatesNoDupes.includes(oneState)) {
-                allStatesNoDupes.push(oneState);
-            }
-        });
-
-        var selector = document.getElementById("stateSelect");
-
-        allStatesNoDupes.sort();
-        allStatesNoDupes.forEach(aState => {
-
-            var newOption = document.createElement("option");
-            newOption.innerHTML = aState;
-            selector.append(newOption);
+            myFirstVueObject(jsonData.results[0].members)
         })
-    })
-//});
+}
+
 
 function myFirstVueObject(myData) {
 
@@ -57,11 +31,10 @@ function myFirstVueObject(myData) {
                 filteredMembers: [],
                 checkedNames: [],
                 stateNameArray: [],
-                stateValue: ''
+                stateValue: 'all'
             },
             created() {
-                console.log("in created")
-                this.createStateNameArray()
+                this.createStateNameArray();
             },
             methods: {
                 partyFilter: function () {
@@ -86,48 +59,3 @@ function myFirstVueObject(myData) {
             }
     })
 }
-
-    //function handleChange() {
-    //
-    //            myTBody.innerHTML = "";
-    //
-    //            let myChecked = Array.from(document.querySelectorAll('input.party:checked'));
-    //
-    //            let myCheckedValues = [];
-    //            myChecked.forEach(checkbox => {
-    //                myCheckedValues.push(checkbox.value);
-    //            });
-    //state selector
-    //            let selector = document.getElementById("stateSelect");
-    //
-    //
-    //            data.results[0].members.forEach(member => {
-    //
-    //                let partyFilter = myCheckedValues.includes(member.party) || myCheckedValues.length == 0;
-    //                let statesFilter = (member.state == selector.value) || selector.value == "all";
-    //
-    //                if (partyFilter && statesFilter) {
-    //                    var myTr = document.createElement("tr");
-    //                    var names = [member.first_name, member.middle_name, member.last_name];
-    //                    var full_name = names.join(" ");
-    //                    var url = member.url;
-    //
-    //                    myTr.insertCell().innerHTML = full_name.link(url);
-    //                    myTr.insertCell().innerHTML = member.party;
-    //                    myTr.insertCell().innerHTML = member.state;
-    //                    myTr.insertCell().innerHTML = member.seniority;
-    //                    myTr.insertCell().innerHTML = member.votes_with_party_pct + '%';
-    //                    myTBody.append(myTr);
-    //                }
-    //            });
-    //
-    //            myTable.append(myTBody);
-    //
-    //
-    //        }
-
-
-    //function filterByParty() {
-    //  return document.getElementsByClassName("party").checked;
-    //}
-    //document.getElementById("senate-data").innerHTML = JSON.stringify(data,null,2);
